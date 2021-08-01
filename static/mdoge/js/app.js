a6c20f487643947825b1fbf35806ae0b7bfe4673
ganiyu <odunaiyakola@gmail.com>
2021-08-01 19:28:07 +0100

    newly edit

--- static/mdoge/js/app.js
+++ static/mdoge/js/app.js
@@ -1,7 +1,22 @@
 $(document).ready(function () {
     $('.cadd').on('click', function (e) {
-        
+
 
     });
-
+    $('body').on('click', function () {
+        if ($("#menu").is(':checked')) {
+            $('body').addClass("lock-body")
+        }
+        else {
+            $('body').removeClass("lock-body")
+        }
+    })
+    $(window).on('resize', function () {
+        screen = $(window.innerWidth)
+        console.log($("#menu").prop('checked'))
+        if ($("#menu").is(':checked') && screen[0] > 790) {
+            $("#menu").prop('checked', false)
+            $('body').removeClass("lock-body")
+        }
+    })
 })
