8d8ef54df3a6a072c51be1c9bbb6a5d963534dd6
ganiyu <odunaiyakola@gmail.com>
2021-08-03 12:09:45 +0100

    newly edit 6

--- static/mdoge/js/app.js
+++ static/mdoge/js/app.js
@@ -1,8 +1,4 @@
 $(document).ready(function () {
-    $('.cadd').on('click', function (e) {
-
-
-    });
     $('body').on('click', function () {
         if ($("#menu").is(':checked')) {
             $('body').addClass("lock-body")
@@ -13,10 +9,50 @@ $(document).ready(function () {
     })
     $(window).on('resize', function () {
         screen = $(window.innerWidth)
-        console.log($("#menu").prop('checked'))
         if ($("#menu").is(':checked') && screen[0] > 790) {
             $("#menu").prop('checked', false)
             $('body').removeClass("lock-body")
         }
     })
+    let launchDate = new Date("Aug 7, 2021 1:40:00").getTime();
+
+    // Setup Timer to tick every 1 second
+    let timer = setInterval(tick, 1000);
+
+    function tick() {
+        // Get current time
+        let now = new Date().getTime();
+        // Get the difference in time to get time left until reaches 0
+        let t = launchDate - now;
+
+        // Check if time is above 0
+        if (t > 0) {
+            // Setup Days, hours, seconds and minutes
+            // Algorithm to calculate days...
+            let days = Math.floor(t / (1000 * 60 * 60 * 24));
+            // prefix any number below 10 with a "0" E.g. 1 = 01
+            if (days < 10) { days = "0" + days; }
+
+            // Algorithm to calculate hours
+            let hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            if (hours < 10) { hours = "0" + hours; }
+
+            // Algorithm to calculate minutes
+            let mins = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+            if (mins < 10) { mins = "0" + mins; }
+
+            // Algorithm to calc seconds
+            let secs = Math.floor((t % (1000 * 60)) / 1000);
+            if (secs < 10) { secs = "0" + secs; }
+
+            // Create Time String
+            let time = `${days} : ${hours} : ${mins} : ${secs}`;
+
+            // Set time on document
+            $('.countdown').html(time);
+            console.log(launchDate)
+
+        }
+    }
+
 })
