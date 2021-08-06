3e0e09828912242fe66a2a5d9995e037697ecc67
ganiyu <odunaiyakola@gmail.com>
2021-08-06 21:19:49 +0100

    added timer

--- static/mdoge/js/app.js
+++ static/mdoge/js/app.js
@@ -14,7 +14,7 @@ $(document).ready(function () {
             $('body').removeClass("lock-body")
         }
     })
-    let launchDate = new Date("Aug 6, 2021 12:30:00").getTime();
+    let launchDate = new Date("Aug 8, 2021 12:30:00").getTime();
 
     // Setup Timer to tick every 1 second
     let timer = setInterval(tick, 1000);
@@ -26,33 +26,40 @@ $(document).ready(function () {
         let t = launchDate - now;
 
         // Check if time is above 0
+        let timezone = []
+        let spans = []
         if (t > 0) {
             // Setup Days, hours, seconds and minutes
             // Algorithm to calculate days...
             let days = Math.floor(t / (1000 * 60 * 60 * 24));
             // prefix any number below 10 with a "0" E.g. 1 = 01
             if (days < 10) { days = "0" + days; }
-
+            timezone.push(days);
             // Algorithm to calculate hours
             let hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
             if (hours < 10) { hours = "0" + hours; }
-
+            timezone.push(hours)
             // Algorithm to calculate minutes
             let mins = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
             if (mins < 10) { mins = "0" + mins; }
+            timezone.push(mins)
 
             // Algorithm to calc seconds
             let secs = Math.floor((t % (1000 * 60)) / 1000);
             if (secs < 10) { secs = "0" + secs; }
+            timezone.push(secs)
 
             // Create Time String
             let time = `${days} : ${hours} : ${mins} : ${secs}`;
 
             // Set time on document
-            $('.countdown').html(time);
-            console.log(launchDate)
-
+            timezone.map(function (value, index) {
+                $('.countdown span').each(function (indexs, values) {
+                    spans.push(values)
+                })
+                return $(spans).eq(index).html(value)
+            })
         }
+        return 
     }
-
 })
